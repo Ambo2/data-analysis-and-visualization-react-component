b6c4530a970133c7539982238bbef38a586204e1
Franz-Eric Sill <107982913+qwerdenkerXD@users.noreply.github.com>
2025-01-27 13:52:43 +0100

    removed unused code

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import UserInterface from './UserInterface';
-import NumericValidator from './NumericValidator';
-import ParableAnalyzer from './ParableAnalyzer';
-import D3LinePlot from './D3LinePlot';
 
 /**
  * Example usage of the DataAnalysisAndVisualization component with a D3LinePlot visualizer.
